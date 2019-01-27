0acac91646d6caaaf7b63dcc58931f58457884d5
cyangoon <crystalcyst@gmail.com>
2019-01-27 12:05:25 -0800

    uh oh

--- Assets/Scripts/Interactible.cs
+++ Assets/Scripts/Interactible.cs
@@ -19,4 +19,10 @@ public class Interactible : DungeonObject {
     {
 
     }
+
+    public void interact()
+    {
+
+
+    }
 }
---
+++ Assets/Scripts/StoneDoor.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneDoor : Interactible
+{
+
+    Animator animator;
+    public MeshCollider front;
+    public MeshCollider back;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("e"))
+        {
+            if (!animator.GetBool("isOpen"))
+            {
+                open();
+            } else {
+            if (animator.GetBool("isOpen"))
+                {
+                    close();
+                }
+            }
+        }
+    }
+
+        
+
+    public StoneDoor(int x, int y, int z)
+        : base(x, y, z)
+    {
+
+    }
+
+    public void open()
+    {
+        animator.SetTrigger("onInteract");
+        animator.SetBool("isOpen", true);
+
+    }
+
+    public void close()
+    {
+        animator.SetTrigger("onInteract");
+        animator.SetBool("isOpen", false);
+
+    }
+
+    public void accessModulate()
+    {
+        if (front.enabled == false)
+        {
+            front.enabled = true;
+            back.enabled = true;
+        } else
+        {
+            if (front.enabled == true)
+            {
+                front.enabled = false;
+                back.enabled = false;
+            }
+        }
+    }
+
+}
