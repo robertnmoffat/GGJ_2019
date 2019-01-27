c5b82ace8fd8620c8415b207eea58756779778d6
cyangoon <crystalcyst@gmail.com>
2019-01-26 22:04:20 -0800

    player health and sanity etc

---
+++ Assets/Scripts/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalConstants {
+
+    public const string GAME_START_MESSAGE = "GREETINGS MORTAL... /Are you ready to die?/";
+    public const string DEATH_MESSAGE = "Now that's Italian!";
+    public const string LEVELUP_MESSAGE = "Now that's Italian!";
+
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,6 +8,15 @@ public class PlayerScript : MonoBehaviour {
 
     public Vector3 direction;
 
+    public const int INIT_MAX_HP = 100;
+    public const int INIT_MAX_SANITY = 0;
+
+    private int maxHealth;
+    private int currentHealth;
+    private int maxSanity;
+    private int currentSanity;
+
+
     // Use this for initialization
     void Start () {
         playerInput.playerTransform = transform;
@@ -20,5 +29,65 @@ public class PlayerScript : MonoBehaviour {
         direction = transform.eulerAngles;
     }
 
-    
+
+    public PlayerScript()
+    {
+        maxHealth = 10;
+
+
+    }
+
+    public int MaxHealth
+    {
+        get
+        {
+            return maxHealth;
+        }
+
+        set
+        {
+            maxHealth = value;
+        }
+    }
+
+    public int CurrentHealth
+    {
+        get
+        {
+            return currentHealth;
+        }
+
+        set
+        {
+            currentHealth = value;
+        }
+    }
+
+    public int MaxSanity
+    {
+        get
+        {
+            return maxSanity;
+        }
+
+        set
+        {
+            maxSanity = value;
+        }
+    }
+
+    public int CurrentSanity
+    {
+        get
+        {
+            return currentSanity;
+        }
+
+        set
+        {
+            currentSanity = value;
+        }
+    }
+
+
 }
