721ef3bc175c519726c73ef7eecc91647292b8ec
cyangoon <crystalcyst@gmail.com>
2019-01-27 16:09:03 -0800

    sizing door, added space check

--- Assets/Scripts/StoneDoor.cs
+++ Assets/Scripts/StoneDoor.cs
@@ -7,36 +7,34 @@ public class StoneDoor : MonoBehaviour {
     Animator animator;
     public MeshCollider front;
     public MeshCollider back;
+    public PlayerScript player;
 
     // Use this for initialization
     void Start()
     {
         animator = GetComponent<Animator>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyUp("e"))
-        {
-            if (!animator.GetBool("isOpen"))
-            {
-                open();
-            }
-            else
-            {
-                if (animator.GetBool("isOpen"))
+            if (checkDistance()) {
                 {
-                    close();
+                    if (!animator.GetBool("isOpen"))
+                    {
+                        open();
+                    }
+                    else
+                    {
+                        if (animator.GetBool("isOpen"))
+                        {
+                            close();
+                        }
+                    }
                 }
-            }
         }
     }
-
-
-
-
 
 public void open()
 {
@@ -70,4 +68,18 @@ public void accessModulate()
         }
     }
 }
+
+    public bool checkDistance()
+    {
+
+       float distance = Vector3.Distance(this.transform.position, player.transform.position);
+
+        if (distance <= 0.5) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
